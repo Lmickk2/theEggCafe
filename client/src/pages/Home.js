5450c729096436592cf5e389e08ee92995274679
Lmickk2 <lukemickan21@icloud.com>
2023-04-06 02:57:47 -0400

    more media queries, added new background for smaller screens

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,17 +1,21 @@
 import { Link } from "react-router-dom";
-import background from "../images/Egg.png"
+import backgroundSmall from "../images/EggFoodBG.png"
+import backgroundLarge from "../images/Egg.png"
 
 function Home() {
-    return(
-        <div>
-    <div className="img-display">
-    <img src={background} className="showcase" alt=""/>
-    <h1 className="welcome">Welcome to the Egg!</h1>
-    <p className="desc">- Good Food, Good Times -</p>
-    <Link to ="/menu"><button className="view-menu">View Menu</button></Link>
-  </div>
- </div>
-    )
+  const screenWidth = window.innerWidth;
+  const backgroundImage = screenWidth < 1024 ? backgroundSmall : backgroundLarge;
+
+  return (
+    <div>
+      <div className="img-display">
+        <img src={backgroundImage} className="showcase" alt=""/>
+        <h1 className="welcome">Welcome to the Egg!</h1>
+        <p className="desc">Good Food, Good Times</p>
+        <Link to ="/menu"><button className="view-menu">View Menu</button></Link>
+      </div>
+    </div>
+  );
 }
 
 export default Home;
