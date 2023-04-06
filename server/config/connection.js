b7adb10b08f64f4013d45e35261dd71c320feeee
Lmickk2 <lukemickan21@icloud.com>
2023-04-05 20:34:50 -0400

    heroku

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,10 +1,12 @@
 const mongoose = require('mongoose');
 
 mongoose.connect(
-  process.env.MONGODB_URI || 'mongodb+srv://lukemickan21:DKaXXjVmymncETLe@theeggcluster.l1cc94l.mongodb.net/?retryWrites=true&w=majority',
+  process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/theEggCafe',
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
   }
 );
 
